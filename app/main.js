726113e2719d43b8c9659a5bcc72893a7ac7f71f
vaibhav1me <vaibhav1@me.iitr.ac.in>
2025-08-27 15:33:49 +0530

    handling invalid commands

--- app/main.js
+++ app/main.js
@@ -7,5 +7,6 @@ const rl = readline.createInterface({
 
 // Uncomment this block to pass the first stage
 rl.question("$ ", (answer) => {
+  console.log(`${answer}: command not found`)
   rl.close();
 });
