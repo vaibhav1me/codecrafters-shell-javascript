b674cdaefe7511d47eff23e1e577817dfe3fa84c
vaibhav1me <vaibhav1@me.iitr.ac.in>
2025-08-27 15:43:21 +0530

    REPL

--- app/main.js
+++ app/main.js
@@ -5,8 +5,11 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// Uncomment this block to pass the first stage
-rl.question("$ ", (answer) => {
-  console.log(`${answer}: command not found`)
-  rl.close();
-});
+const prompt = () => {
+  rl.question("$ ", (answer) => {
+    console.log(`${answer}: command not found`)
+    prompt();
+  });
+}
+
+prompt();
