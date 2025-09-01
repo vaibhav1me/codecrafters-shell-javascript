1d2ba1d17e8097210ad22da418f6ef7d995e0045
vaibhav1me <vaibhav1@me.iitr.ac.in>
2025-09-01 14:54:32 +0530

    added echo builtin

--- app/main.js
+++ app/main.js
@@ -9,6 +9,10 @@ const prompt = () => {
   rl.question("$ ", (answer) => {
     if (answer == "exit 0") {
       rl.close();
+    } else if (answer.startsWith("echo ")) {
+      const message = answer.slice(5);
+      console.log(message);
+      prompt();
     } else {
       console.log(`${answer}: command not found`);
       prompt();
