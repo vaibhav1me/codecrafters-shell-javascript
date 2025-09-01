97bbb9ffdb3b1b5ba64a9e7b64263485cbb7880c
vaibhav1me <vaibhav1@me.iitr.ac.in>
2025-09-01 14:58:19 +0530

    added type builtins

--- app/main.js
+++ app/main.js
@@ -5,6 +5,8 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const builtIns = ["echo", "exit", "type"];
+
 const prompt = () => {
   rl.question("$ ", (answer) => {
     if (answer == "exit 0") {
@@ -12,6 +14,14 @@ const prompt = () => {
     } else if (answer.startsWith("echo ")) {
       const message = answer.slice(5);
       console.log(message);
+      prompt();
+    } else if (answer.startsWith("type ")) {
+      const message = answer.slice(5);
+      if (builtIns.includes(message)) {
+        console.log(`${message} is a shell builtin`);
+      } else {
+        `${message}: not found`;
+      }
       prompt();
     } else {
       console.log(`${answer}: command not found`);
